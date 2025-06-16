d8e6acbfb6cf2a29a92ba5db64029453a0622e78
@S52000216 <52000216@student.tdtu.edu.vn>
2025-06-16 14:02:58 +0700

    fix: Rename Dockerfile.txt to Dockerfile for Railway to detect it

--- obj/Debug/net8.0/DecalXeAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DecalXeAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DecalXeAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9696ba5f53e327d2af392f84ce4892b1a66eda45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+706fe1118dcd4ab3f2a680b9a90b7e2807033c66")]
 [assembly: System.Reflection.AssemblyProductAttribute("DecalXeAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DecalXeAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
