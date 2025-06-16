706fe1118dcd4ab3f2a680b9a90b7e2807033c66
@S52000216 <52000216@student.tdtu.edu.vn>
2025-06-16 13:59:59 +0700

    feat: Add Dockerfile to enforce .NET 8.0 SDK for Railway deployment

--- obj/Debug/net8.0/DecalXeAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DecalXeAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DecalXeAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8df326b45a98c32f9375c08517f7705aafa4ee9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9696ba5f53e327d2af392f84ce4892b1a66eda45")]
 [assembly: System.Reflection.AssemblyProductAttribute("DecalXeAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DecalXeAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
