90d9d26c47aaa42659b5eec416ab47064251374e
Mai Nguyễn Tuấn Tú <tuantumn.0103@gmail.com>
2025-07-02 14:09:56 +0700

    feat: refractor code

--- Models/Store.cs
+++ Models/Store.cs
@@ -13,7 +13,6 @@ namespace DecalXeAPI.Models
         public string StoreName { get; set; } = string.Empty;
 
         [MaxLength(255)] // Địa chỉ có thể dài
-        [JsonIgnore]
         public string? Address { get; set; } // Địa chỉ có thể cho phép NULL nếu chưa cập nhật
 
         // Navigation Property: Một Store có thể có nhiều Employee.
